3c1c177756100451d1dc182f4ea35f5d04eb15ba
hbalenda <hbalenda@gmail.com>
2020-04-17 19:58:37 -0700

    Start full screen

---
+++ docs/flipbook.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const clientHeight = window.innerHeight;
+    const clientWidth = window.innerWidth;
+
+    const setFlipBookDimensions = function() {
+        const clientHeight = window.innerHeight;
+        const clientWidth = window.innerWidth;
+        const margin = '10';
+        const doublemargin = '20';
+        const singlePageWidth = clientWidth /2;
+        document.querySelector('#flipbook').style.height = clientHeight;
+        document.querySelector('#flipbook').style.width = clientWidth;
+        document.querySelector('#flipbook .page').style.height = clientHeight;
+        document.querySelector('#flipbook .page').style.width = clientWidth;
+        document.querySelector('.video-embed').style.width = singlePageWidth;
+        document.querySelector('.video-embed').style.height = clientHeight;
+        document.querySelector('.qzine-page').style.width = singlePageWidth;
+        document.querySelector('.qzine-page').style.height = clientHeight;
+        document.querySelector('.video-embed iframe').style.height = clientHeight;
+        document.querySelector('.video-embed iframe').style.width = singlePageWidth;
+        
+    }
+    //Initialize FlipBook
+    $("#flipbook").turn({
+        width: clientWidth,
+        height: clientHeight,
+        page: 8,
+        autoCenter: true,
+    });
+    //Set Flipbook Dimensions
+    setFlipBookDimensions();
+    //Bind Click Events
+    $("#previous").click(function() {
+        $("#flipbook").turn("previous");
+    });
+    
+    $("#next").click(function() {
+        $("#flipbook").turn("next");
+    });
+
+    //Bind Resize Events
+    window.addEventListener('onresize', function() {
+        setFlipBookDimensions();
+    })
+})
